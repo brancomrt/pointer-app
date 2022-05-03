199e9327381ef961e34fc2b3180bf93b452fae6b
Alex Martins <brancomrt@gmail.com>
2022-05-02 21:09:41 -0300

    Autalizacao dependencias.sh

--- dependencias.sh
+++ dependencias.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo apt update -y
+sudo apt-get update 
+sudo apt-get install git ansible -y
+
+git clone https://github.com/brancomrt/pointer-app.git /tmp/pointer-app/
+
+cd /tmp/pointer-app/
+ansible-playbook pointer-playbook.yml
